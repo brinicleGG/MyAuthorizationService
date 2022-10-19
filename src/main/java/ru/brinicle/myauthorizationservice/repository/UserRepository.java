7c120f54217bd312b5a5476ec11e5b0b82c45a13
brinicleGG <GG9DJM@GMAIL.COM>
2022-10-20 00:42:33 +0300

    add UserDetailsServiceImpl

--- src/main/java/ru/brinicle/myauthorizationservice/repository/UserRepository.java
+++ src/main/java/ru/brinicle/myauthorizationservice/repository/UserRepository.java
@@ -3,7 +3,10 @@ package ru.brinicle.myauthorizationservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.brinicle.myauthorizationservice.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByEmail(String email);
 
 }
